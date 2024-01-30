b09d0dbb82e42be30334d3ef208fd2b9fbcb2f84
RajaPriyanka <122675666+rajapriyanka@users.noreply.github.com>
2024-01-30 19:35:58 +0530

    SQL File

---
+++ library_ms.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2024 at 03:05 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_ms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_details`
+--
+
+CREATE TABLE `book_details` (
+  `book_id` int(11) NOT NULL,
+  `book_name` varchar(250) DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `book_details`
+--
+
+INSERT INTO `book_details` (`book_id`, `book_name`, `author`, `quantity`) VALUES
+(1, 'JAVA', 'PUNTAMBEKAR', 8),
+(2, 'Foundation Of DataScience', 'I A DHOTRE', 4),
+(5, 'jk', 'nn', 34),
+(6, 'Better', 'Hamy', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue_book_details`
+--
+
+CREATE TABLE `issue_book_details` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `book_name` varchar(150) DEFAULT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `student_name` varchar(150) DEFAULT NULL,
+  `issue_date` date DEFAULT NULL,
+  `due_date` date DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `issue_book_details`
+--
+
+INSERT INTO `issue_book_details` (`id`, `book_id`, `book_name`, `student_id`, `student_name`, `issue_date`, `due_date`, `status`) VALUES
+(1, 1, 'JAVA', 1, 'Priya', '2024-01-02', '2024-01-16', 'returned');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_details`
+--
+
+CREATE TABLE `student_details` (
+  `student_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `course` varchar(100) DEFAULT NULL,
+  `branch` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_details`
+--
+
+INSERT INTO `student_details` (`student_id`, `name`, `course`, `branch`) VALUES
+(1, 'Priya', 'B.E', 'CSE\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `contact` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `email`, `contact`) VALUES
+(1, 'abi', '12345', 'abi@gmail.com', '123456789'),
+(2, '', '', '', ''),
+(3, 'agdja', '267432627', 'sahhdjahah', 'gasakshdj'),
+(4, 'abi', '1245', 'dhfhkjs@gmail.com', '9876543210'),
+(5, 'Priya', '1234', 'PRIYA@GMAIL.COM', '9876543210');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_details`
+--
+ALTER TABLE `book_details`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `issue_book_details`
+--
+ALTER TABLE `issue_book_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_details`
+--
+ALTER TABLE `student_details`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_details`
+--
+ALTER TABLE `book_details`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `issue_book_details`
+--
+ALTER TABLE `issue_book_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `student_details`
+--
+ALTER TABLE `student_details`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
